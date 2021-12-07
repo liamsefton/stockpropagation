7e14c80288c634ac796fccc83544eec240591ca8
liamsefton <88951421+liamsefton@users.noreply.github.com>
2021-12-06 17:14:53 -0800

    Add files via upload

--- miner.py
+++ miner.py
@@ -18,10 +18,10 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 symbols_file = open("constituents_csv.csv", "r")
 symbols = symbols_file.readline().split(",")
-symbols = symbols[90:]
 
 
-f = open("stock_data.txt", "w")
+
+f = open("stock_data_normalized.txt", "w")
 writer = csv.writer(f)
 
 #tickers = ["CNNA", "CKSG", "WRAP", "BBIG", "VPOR", "SVAP", "SPXC", "SOLI", "NWL", "HON", "MLCG", "EFTI", "BORK", "AGTX", "ARCW", "CVR", "CIX", "BOOM", "WIRE", "FRD", "ROCK", "VATE", "MLI", "MATW", "NNBR", "PERT", "SVT", "ROLL", "TLDN", "TKR", "VMI", "AOS", "AVOZ", "APOG", "ARCS", "CPWY", "CNR", "GFF", "TILE", "LGBS", "LGNC", "LYTS", "MAS", "DOOR", "MEGH", "NDDG", "PGTI", "PLPC"]
@@ -41,11 +41,12 @@ for ticker in symbols:
     #data = yfinance.download(ticker,start='2019-03-31',end='2019-04-30')
     #close_data = data['Close'].head(1).to_string()
     #close_data = close_data.split()
+    #REMOVED: TOTAL REVENUE, TOTAL ASSETS
+    #CHANGED: TOTAL LIABILITIES -> NET ASSETS
 
-
-    balance_sheet_list = ['Total Liab','Total Assets','Cash','Total Current Liabilities','Total Current Assets','Net Tangible Assets']
+    balance_sheet_list = ['Total Liab', 'Cash','Total Current Liabilities','Total Current Assets','Net Tangible Assets']
     cash_flow_list = ['Change In Cash','Total Cash From Operating Activities','Dividends Paid']
-    fin_list = ['Research Development','Gross Profit','Total Revenue']
+    fin_list = ['Research Development','Gross Profit']
     price_list = ['Current Price','Increase In Price From 1yr','Increase In Price From 2yr','Increase In Price From 3yr','Increase In Price From 4yr','Increase In Price From 5yr']
     dowj_list = ['DOWJ at current', 'DOWJ increase 1yr', 'DOWJ increase 2yr','DOWJ increase 3yr','DOWJ increase 4yr','DOWJ increase 5yr', "y-value"]
     dates = []
@@ -87,34 +88,89 @@ for ticker in symbols:
         for key2 in attributes_dict_1.keys():
             if key2 in balance_sheet_list:
                 if attributes_dict_1[key2] != None:
-                    value_list_1.append(attributes_dict_1[key2])
-                else:
-                    value_list_1.append(0)
+                    if key2 == "Total Liab":
+                        value_list_1.append(((float(attributes_dict_1["Total Assets"]) -  float(attributes_dict_1[key2])))/float(attributes_dict_1["Total Assets"]))
+                    elif key2 == "Cash" or key2 == "Total Current Assets" or key2 == "Net Tangible Assets":
+                        value_list_1.append(float(attributes_dict_1[key2])/float(attributes_dict_1["Total Assets"]))
+                    elif key2 == "Total Current Liabilities":
+                        value_list_1.append(float(attributes_dict_1[key2])/float(attributes_dict_1["Total Liab"]))
+
+
 
         attributes_dict_2 = cf_dict[key]
+        attributes_dict_3 = fin_dict[key]
         value_list_2 = []
         for key3 in attributes_dict_2.keys():
             if key3 in cash_flow_list:
                 if attributes_dict_2[key3] != None:
-                    value_list_2.append(attributes_dict_2[key3])
-                else:
-                    value_list_2.append(0)
+                    if key3 == "Change In Cash" or key3 == "Total Cash From Operating Activities" or key3 == "Dividends Paid":
+                        value_list_2.append(float(attributes_dict_2[key3])/float(attributes_dict_3["Gross Profit"]))
 
-        attributes_dict_3 = fin_dict[key]
+
         value_list_3 = []
         for key4 in attributes_dict_3.keys():
             if key4 in fin_list:
                 if attributes_dict_3[key4] != None:
-                    value_list_3.append(attributes_dict_3[key4])
-                else:
-                    value_list_3.append(0)
-        
+                    try:
+                        if key4 == "Gross Profit":
+                            value_list_3.append(float(attributes_dict_3[key4])/float(attributes_dict_3["Total Revenue"]))
+                        elif key4 == "Research Development":
+                            value_list_3.append(float(attributes_dict_3[key4])/float(attributes_dict_3["Total Revenue"]))
+                    except:
+                        print("nice try dirty data")
+
+        count = 0
         value_list_4 = []
         value_list_5 = []
-        for i in range(6):
+        date = dates[count]
+        next_day = date + timedelta(days=10)
+        prev_day = date - timedelta(days=10)
+        new_date1 = date.strftime(date_format)
+        new_date2 = next_day.strftime(date_format)
+        data = yfinance.download(ticker,start=new_date1,end=new_date2)
+        dowj_data = yfinance.download('SPY',start=new_date1,end=new_date2)
+
+        valid_line = True
+
+        while data.empty:
+            if interrupted:
+                valid_line = False
+                interrupted = False
+                break
+            next_day += timedelta(days=10)
+            prev_day -= timedelta(days=10)
+            new_date2 = next_day.strftime(date_format)
+            data = yfinance.download(ticker,start=new_date1,end=new_date2)
+            if data.empty:
+                new_date2 = next_day.strftime(date_format)
+                data = yfinance.download(ticker,start=new_date2,end=new_date1)
+
+
+        while dowj_data.empty:
+            if interrupted:
+                valid_line = False
+                interrupted = False
+                break
+            next_day += timedelta(days=10)
+            prev_day -= timedelta(days=10)
+            new_date2 = next_day.strftime(date_format)
+            data = yfinance.download("SPY",start=new_date1,end=new_date2)
+            if data.empty:
+                new_date2 = next_day.strftime(date_format)
+                data = yfinance.download("SPY",start=new_date2,end=new_date1)
+
+        if valid_line:
+            close_data = data['Close'].head(1).to_string()
+            close_data = close_data.split()
+            dowj_close_data = dowj_data['Close'].head(1).to_string()
+            dowj_close_data = dowj_close_data.split()
+
+        current_price = float(close_data[2])
+        current_dowj_price = float(dowj_close_data[2])
+
+        for i in range(5):
             print("*")
-            if i == 0:
-                date = dates[count]
+            date = date - timedelta(days=365)
             next_day = date + timedelta(days=10)
             prev_day = date - timedelta(days=10)
             new_date1 = date.strftime(date_format)
@@ -135,7 +191,7 @@ for ticker in symbols:
                 data = yfinance.download(ticker,start=new_date1,end=new_date2)
                 if data.empty:
                     new_date2 = next_day.strftime(date_format)
-                    data = yfinance.download(ticker,start=new_date1,end=new_date2)
+                    data = yfinance.download(ticker,start=new_date2,end=new_date1)
 
 
             while dowj_data.empty:
@@ -149,7 +205,7 @@ for ticker in symbols:
                 data = yfinance.download("SPY",start=new_date1,end=new_date2)
                 if data.empty:
                     new_date2 = next_day.strftime(date_format)
-                    data = yfinance.download("SPY",start=new_date1,end=new_date2)
+                    data = yfinance.download("SPY",start=new_date2,end=new_date1)
 
             if valid_line:
                 close_data = data['Close'].head(1).to_string()
@@ -157,9 +213,8 @@ for ticker in symbols:
                 dowj_close_data = dowj_data['Close'].head(1).to_string()
                 dowj_close_data = dowj_close_data.split()
 
-                value_list_4.append(close_data[2])
-                value_list_5.append(dowj_close_data[2])
-                date = date - timedelta(days=365)
+                value_list_4.append(float(close_data[2])/current_price)
+                value_list_5.append(float(dowj_close_data[2])/current_dowj_price)
 
         count += 1
         
